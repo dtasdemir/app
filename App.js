61a2cbf31b738780e063bf95a07f44d49bbde07c
derya <derya@aprilerp.com>
2023-08-09 21:45:54 +0300

    Gerekli olan diğer paketleri yükledim.

--- App.js
+++ App.js
@@ -1,24 +1,31 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Products from "./src/screens/Products";
-import ProductDetail from "./src/screens/ProductDetail";
+import Products from './src/screens/Products';
+import ProductDetail from './src/screens/ProductDetail';
 
-const Stack = createStackNavigator();
+import m from './src/style/MStyle';
+import {Provider, useDispatch} from 'react-redux';
+
+import store from './src/redux/index';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer >
-            <Stack.Navigator
-            screenOptions={{
-                headerShown: false,
-            }}
-        >
-            <Stack.Screen name="products" component={Products} />
-            <Stack.Screen name="productsDetail" component={ProductDetail} />
-
+    
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            statusBarColor: m.primaryColor,
+          }}>
+          <Stack.Screen name="products" component={Products} />
+          <Stack.Screen name="productsDetail" component={ProductDetail} />
         </Stack.Navigator>
-        </NavigationContainer>
-    )
+      </NavigationContainer>
+    </Provider>
+  );
 }
