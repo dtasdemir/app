5a6554ee1665babdbd071e5fcd3c9590d7202519
derya <derya@aprilerp.com>
2023-08-09 09:36:20 +0300

    Axios and redux packages uploaded and -navigation- folder deleted

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
-import MainNavigator from "./src/navigations/MainNavigator";
+
 import Products from "./src/screens/Products";
 import ProductDetail from "./src/screens/ProductDetail";
 
